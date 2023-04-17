9250312715d6bb16078d3b1c36044db619cf35d3
Musharraf Dudekula <musharrafdudekula@gmail.com>
2023-04-17 10:51:58 +0530

    Assignment 4

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,2 +1,12 @@
-var a = "hello";
-console.log(a);
+(function() {
+	var names = ["John", "Jack", "Mark", "Bill", "Elon", "Erik", "Yaakov", "Jim", "Nick", "Tony"];
+	for (var i = 0; i < names.length; i++) {
+		var firstLetter = names[i].charAt(0).toLowerCase();
+		if (firstLetter === 'j') {
+			byeSpeaker.speak(names[i]);
+		}
+		else {
+			helloSpeaker.speak(names[i]);
+		}
+	}
+})();
