da6de3be9fc14e67644dd61971a1c8fc4d2e6b6e
Musharraf Dudekula <musharrafdudekula@gmail.com>
2023-04-17 11:00:45 +0530

    Assignment 4

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,5 +1,5 @@
 (function() {
-	var names = ["John", "Jack", "Mark", "Bill", "Elon", "Erik", "Yaakov", "Jim", "Nick", "Tony"];
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var i = 0; i < names.length; i++) {
 		var firstLetter = names[i].charAt(0).toLowerCase();
 		if (firstLetter === 'j') {
